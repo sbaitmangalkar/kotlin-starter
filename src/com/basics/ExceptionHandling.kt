43444260c8549c886351eb11d722d5951ef3485d
Shyam <catch.shyambaitmangalkar@gmail.com>
2018-01-23 15:09:25 +0530

    Adding exception handling code

--- src/com/basics/ExceptionHandling.kt
+++ src/com/basics/ExceptionHandling.kt
@@ -4,6 +4,9 @@ fun main(args: Array<String>) {
     println("Enter a dividend and a divisor with a \\\"space\\\" in between")
     val(dividend, divisor) = readLine()!!.split(' ').map(String::toInt)
 
+    /**
+     * A classic divide by zero example
+     */
     try {
         if(divisor == 0)
             throw IllegalArgumentException("Can't divide by zero!!")
