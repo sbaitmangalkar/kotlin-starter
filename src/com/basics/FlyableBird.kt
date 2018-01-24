89e240b975ee26727284232a4505803ee9ebff3b
Shyam <catch.shyambaitmangalkar@gmail.com>
2018-01-24 18:26:31 +0530

    Adding more information about interfaces

--- src/com/basics/FlyableBird.kt
+++ src/com/basics/FlyableBird.kt
@@ -1,9 +1,11 @@
 package com.basics
 
+//Simple Interface
 interface FlyableBird {
     fun fly() : Unit
 }
 
+//Implementation
 class Bird(val name : String, var distanceFlown : Double) : FlyableBird {
     override fun fly() {
         println("$name can fly up to $distanceFlown mts!")
