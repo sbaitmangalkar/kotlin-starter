3c343830ba1f774713e27a12769eed60c1f7cda3
Shyam <catch.shyambaitmangalkar@gmail.com>
2018-02-13 20:59:11 +0530

    null safety explained

--- src/com/basics/NullSafety.kt
+++ src/com/basics/NullSafety.kt
@@ -1,5 +1,10 @@
 package com.basics
 
+/**
+ * By default, null safety is built into Kotlin.
+ * Only on adding a (?) as suffix to the return type, will allow
+ * the method to return a null value.
+ */
 fun returningNull() : String? {
     return null
 }
